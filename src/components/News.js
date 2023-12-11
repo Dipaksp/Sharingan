a4c11eb586c2ff32d7127b3d4d9c12f38a310831
Dipak Panpatil <150575213+Dipaksp@users.noreply.github.com>
2023-12-11 17:04:50 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -5,26 +5,24 @@ import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 const News = (props)=>{
     const [articles, setArticles] = useState([])
-    const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     } 
-    const updateNews = async ()=> {
-        props.setProgress(10);
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`; 
-        setLoading(true)
+    const updateNews = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;    
         let data = await fetch(url);
-        props.setProgress(30);
-        let parsedData = await data.json()
-        props.setProgress(70);
-        setArticles(parsedData.articles)
-        setTotalResults(parsedData.totalResults)
-        setLoading(false)
-        props.setProgress(100);
-    }
+        let parsedData = await data.json();
+        if (Array.isArray(parsedData.articles)) {
+            setArticles(parsedData.articles);
+            setTotalResults(parsedData.totalResults);
+        } else {
+           
+            setArticles([]);
+        }
+    };
 
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - Sharingan`;
@@ -42,8 +40,7 @@ const News = (props)=>{
       return (
         <>
             <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>Sharingan - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-            {loading && <Spinner />}
-            <InfiniteScroll
+            < InfiniteScroll
                 dataLength={articles.length}
                 next={fetchMoreData}
                     hasMore={articles.length !== totalResults}
@@ -52,7 +49,7 @@ const News = (props)=>{
                     <div className="container">
                          
                     <div className="row">
-                        {articles.map((element) => {
+                        {articles && articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
                                 <NewsItem title={element.title ? element.title.slice(0,50) : ""} description={element.description ? element.description.slice(0,88) : ""} 
                                 imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
